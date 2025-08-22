1eb2af3cee4112329e06879a3d8df5f735bb79f1
shiraisuzuka <succhi104@gmail.com>
2025-08-22 16:50:34 +0900

    [Coding] Q&A

---
+++ dist/js/recruit.js
@@ -0,0 +1,10 @@
+(function() {
+  // よくあるアコーディオン開閉
+  // ----------------------------------------------//
+  $(function () {
+    $(".js-accordion-title").on("click", function () {
+      $(this).next().slideToggle(300);
+      $(this).toggleClass("open", 300);
+    });
+  });
+})();
