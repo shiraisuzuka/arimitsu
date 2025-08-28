2803743ac04e4223b4f4e068822683fee8dc517e
shiraisuzuka <succhi104@gmail.com>
2025-08-28 14:35:55 +0900

    [Coding] ポンプのしくみ

---
+++ dist/js/technology.js
@@ -0,0 +1,12 @@
+(function() {
+
+  const splide = new Splide('.p-technology-slide', {
+      perPage: 3,
+      pauseOnHover: false,
+      pagination: false,
+      // gap: '1.8rem',
+  });
+  
+  splide.mount();
+
+})();
