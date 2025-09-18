f1e2e0df0005ee2711672c80500353bd033f541c
shiraisuzuka <succhi104@gmail.com>
2025-09-18 09:07:46 +0900

    [Coding] サイトマップ

---
+++ wp-content/page-sitemap.php
@@ -0,0 +1,213 @@
+<?php get_header(); ?>
+
+<div class="p-lower-mv">
+  <div class="l-section-inner">
+    <?php breadcrumb(); ?>
+    <hgroup class="p-lower-mv-title">
+      <h1>サイトマップ</h1>
+      <p>site map</p>
+    </hgroup>
+  </div>
+</div>
+
+<section class="p-sitemap l-section">
+  <div class="l-section-inner">
+    <div class="p-sitemap-wrap">
+      <div class="p-sitemap-row top">
+        <a href="<?php page_path(); ?>">
+          <h2>トップ</h2>
+        </a>
+      </div>
+      <div class="p-sitemap-row company">
+        <a href="<?php page_path('company'); ?>">
+          <h2>企業情報</h2>
+        </a>
+        <ul class="p-sitemap-list">
+          <li>
+            <a href="<?php page_path('company/about'); ?>">会社概要</a>
+          </li>
+          <li>
+            <a href="<?php page_path('company/history'); ?>">沿革</a>
+          </li>
+        </ul>
+      </div>
+      <div class="p-sitemap-row product">
+        <a href="<?php page_path('product'); ?>">
+          <h2>製品情報</h2>
+        </a>
+        <div class="p-sitemap-product">
+          <ul class="p-sitemap-list">
+            <li>
+              <a href="<?php page_path('product/01'); ?>">背負動力噴霧器</a>
+            </li>
+            <li>
+              <a href="<?php page_path('product/02'); ?>">セット動噴</a>
+            </li>
+            <li>
+              <a href="<?php page_path('product/03'); ?>">動力散布機</a>
+            </li>
+            <li>
+              <a href="<?php page_path('product/04'); ?>">静電ノズル</a>
+            </li>
+            <li>
+              <a href="<?php page_path('product/05'); ?>">静電ノズル専用カート</a>
+            </li>
+            <li>
+              <a href="<?php page_path('product/06'); ?>">オートランナー</a>
+            </li>
+            <li>
+              <a href="<?php page_path('product/07'); ?>">オートタンクカー</a>
+            </li>
+            <li>
+              <a href="<?php page_path('product/08'); ?>">ブームスプレーヤ</a>
+            </li>
+            <li>
+              <a href="<?php page_path('product/09'); ?>">ハウススプレー</a>
+            </li>
+            <li>
+              <a href="<?php page_path('product/10'); ?>">土壌消毒機</a>
+            </li>
+            <li>
+              <a href="<?php page_path('product/11'); ?>">人力散布機/人力噴霧器</a>
+            </li>
+            <li>
+              <a href="<?php page_path('product/12'); ?>">スピードスプレーヤ</a>
+            </li>
+            <li>
+              <a href="<?php page_path('product/13'); ?>">その他</a>
+            </li>
+            <li>
+              <a href="<?php page_path('product/14'); ?>">フォーミング洗浄機</a>
+            </li>
+            <li>
+              <a href="<?php page_path('product/15'); ?>">高圧温水洗浄機</a>
+            </li>
+            <li>
+              <a href="<?php page_path('product/16'); ?>">ミストシステム</a>
+            </li>
+            <li>
+              <a href="<?php page_path('product/17'); ?>">インバーター制御システム</a>
+            </li>
+            <li>
+              <a href="<?php page_path('product/18'); ?>">部品洗浄機</a>
+            </li>
+            <li>
+              <a href="<?php page_path('product/19'); ?>">容器・器具洗浄機</a>
+            </li>
+            <li>
+              <a href="<?php page_path('product/20'); ?>">パレット洗浄機</a>
+            </li>
+            <li>
+              <a href="<?php page_path('product/21'); ?>">プランジャポンプ</a>
+            </li>
+            <li>
+              <a href="<?php page_path('product/22'); ?>">高圧洗浄機（モータ）</a>
+            </li>
+            <li>
+              <a href="<?php page_path('product/23'); ?>">高圧洗浄機（エンジン）</a>
+            </li>
+          </ul>
+          <ul class="p-sitemap-list">
+            <li>
+              <a href="<?php page_path('product/24'); ?>">プランジャーポンプユニット</a>
+            </li>
+            <li>
+              <a href="<?php page_path('product/25'); ?>">ノズル</a>
+            </li>
+            <li>
+              <a href="<?php page_path('product/26'); ?>">配管洗浄ノズル</a>
+            </li>
+            <li>
+              <a href="<?php page_path('product/27'); ?>">環境機器</a>
+            </li>
+            <li>
+              <a href="<?php page_path('catalog'); ?>">カタログダウンロード</a>
+            </li>
+            <li>
+              <a href="https://www.youtube.com/channel/UCIGZY9G9hTRGFn40E016o5Q" target="_blank">動画一覧</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="p-sitemap-row technology">
+        <a href="<?php page_path('technology'); ?>">
+          <h2>技術情報</h2>
+        </a>
+        <ul class="p-sitemap-list">
+          <li>
+            <a href="<?php page_path('technology/classroom01'); ?>">ポンプのしくみ</a>
+          </li>
+          <li>
+            <a href="<?php page_path('technology/classroom02'); ?>">ポンプ（オイル）について</a>
+          </li>
+          <li>
+            <a href="<?php page_path('technology/classroom03'); ?>">調圧弁のしくみ</a>
+          </li>
+          <li>
+            <a href="<?php page_path('technology/classroom04'); ?>">アンローダバルブのしくみ</a>
+          </li>
+          <li>
+            <a href="<?php page_path('technology/classroom05'); ?>">静電付加</a>
+          </li>
+          <li>
+            <a href="<?php page_path('technology/classroom06'); ?>">圧力一定制御</a>
+          </li>
+          <li>
+            <a href="<?php page_path('technology/classroom07'); ?>">フォームクリーニング</a>
+          </li>
+          <li>
+            <a href="<?php page_path('technology/classroom08'); ?>">少量散布</a>
+          </li>
+        </ul>
+      </div>
+      <div class="p-sitemap-row recruit">
+        <a href="<?php page_path('recruit'); ?>">
+          <h2>採用情報</h2>
+        </a>
+        <ul class="p-sitemap-list">
+          <li>
+            <a href="<?php page_path('recruit/recruitment'); ?>">募集要項</a>
+          </li>
+          <li>
+            <a href="<?php page_path('recruit/arimitsu'); ?>">有光を知る</a>
+          </li>
+          <li>
+            <a href="<?php page_path('recruit/session'); ?>">説明会・イベント</a>
+          </li>
+          <li>
+            <a href="<?php page_path('recruit/qanda'); ?>">Q&A</a>
+          </li>
+        </ul>
+      </div>
+      <div class="p-sitemap-row contact">
+        <a href="<?php page_path('contact'); ?>">
+          <h2>お問い合わせ</h2>
+        </a>
+      </div>
+      <div class="p-sitemap-row news">
+        <a href="<?php page_path('news'); ?>">
+          <h2>最新情報</h2>
+        </a>
+      </div>
+      <div class="p-sitemap-row privacypolicy">
+        <a href="<?php page_path('privacypolicy'); ?>">
+          <h2>プライバシーポリシー</h2>
+        </a>
+      </div>
+      <div class="p-sitemap-row sitemap">
+        <a href="<?php page_path('sitemap'); ?>">
+          <h2>サイトマップ</h2>
+        </a>
+      </div>
+      <div class="p-sitemap-row english">
+        <a href="<?php page_path('english'); ?>">
+          <h2>english site</h2>
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php get_template_part('parts/cta'); ?>
+
+<?php get_footer(); ?>
