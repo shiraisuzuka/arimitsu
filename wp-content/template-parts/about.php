e09767bf18438ce1a2ad0fa78829995c31f38d99
shiraisuzuka <succhi104@gmail.com>
2025-09-22 09:24:17 +0900

    [Modify] 事業所・組織図の変更

--- wp-content/template-parts/about.php
+++ wp-content/template-parts/about.php
@@ -85,9 +85,9 @@
           <ul class="p-company-tab c-tab">
             <li class="tab tab-1 is-active">本社</li>
             <li class="tab tab-2">奈良工場</li>
-            <li class="tab tab-3">営業本部</li>
-            <li class="tab tab-4">サービス部</li>
-            <li class="tab tab-5">物流部</li>
+            <li class="tab tab-3">営業拠点</li>
+            <li class="tab tab-4">アフター<br>サービス(修理)</li>
+            <li class="tab tab-5">アフター<br>サービス(部品)</li>
           </ul>
           <div class="p-company-panel">
             <div class="panel tab-1 is-show">
@@ -115,19 +115,19 @@
               <table class="c-table sp-block">
                 <tbody>
                   <tr>
-                    <th>大阪営業所(本社)</th>
+                    <th>大阪</th>
                     <td>〒537-0001 大阪市東成区深江北1-3-7</td>
                   </tr>
                   <tr>
                     <th>電話番号</th>
-                    <td>TEL:06-6973-2020（産業機械）<br>FAX:06-6976-1031（産業機械）<br>TEL:06-6973-2010（農業機械）<br>FAX:06-6973-2074（農業機械）</td>
+                    <td>TEL:06-6973-2020<br>FAX:06-6976-1031<br>TEL:06-6973-2010<br>FAX:06-6973-2074</td>
                   </tr>
                 </tbody>
               </table>
               <table class="c-table sp-block">
                 <tbody>
                   <tr>
-                    <th>海外営業部(本社)</th>
+                    <th>海外</th>
                     <td>〒537-0001 大阪市東成区深江北1-3-7</td>
                   </tr>
                   <tr>
@@ -145,8 +145,8 @@
                     <td>〒636-0234 奈良県磯城郡田原本町蔵堂553</td>
                   </tr>
                   <tr>
-                    <th>電話番号</th>
-                    <td>TEL:0744-33-4781（製造部）<br>FAX:0744-33-3681（製造部）</td>
+                    <th>代表</th>
+                    <td>TEL:0744-33-4781<br>FAX:0744-33-3681</td>
                   </tr>
                   <tr>
                     <th>アクセス</th>
@@ -163,108 +163,96 @@
               <table class="c-table sp-block">
                 <tbody>
                   <tr>
-                    <th>北海道営業所</th>
+                    <th>北海道</th>
                     <td>〒060-0031 北海道札幌市中央区北一条東12-22-45<a href="https://maps.app.goo.gl/L3nbC3w4rcMqbaDAA" target="_blank">【マップ】</a></td>
                   </tr>
                   <tr>
                     <th>電話番号</th>
-                    <td>TEL:011-222-4904（産業機械）<br>TEL:011-221-7295（農業機械）<br>FAX:011-221-6875（共通）</td>
+                    <td>TEL:011-222-4904<br>TEL:011-221-7295<br>FAX:011-221-6875</td>
                   </tr>
                 </tbody>
               </table>
               <table class="c-table sp-block">
                 <tbody>
                   <tr>
-                    <th>仙台営業所・東北営業所</th>
+                    <th>東北</th>
                     <td>〒983-0043 宮城県仙台市宮城野区萩野町3-2-16<a href="https://maps.app.goo.gl/SaGfsCLS86NHenp37" target="_blank">【マップ】</a></td>
                   </tr>
                   <tr>
                     <th>電話番号</th>
-                    <td>TEL:022-283-6910（産業機械）<br>TEL:022-283-6901（農業機械）<br>FAX:022-283-6909（共通）</td>
+                    <td>TEL:022-283-6910<br>TEL:022-283-6901<br>FAX:022-283-6909</td>
                   </tr>
                 </tbody>
               </table>
               <table class="c-table sp-block">
                 <tbody>
                   <tr>
-                    <th>東京営業所</th>
+                    <th>東京</th>
                     <td>〒101-0032 東京都千代田区岩本町2-15-10 N.Bビル７階<a href="https://maps.app.goo.gl/Xp5Yv4PjGUpGwfkP9" target="_blank">【マップ】</a></td>
                   </tr>
                   <tr>
                     <th>電話番号</th>
-                    <td>TEL:03-5835-3974（産業機械）<br>FAX:03-3865-3086（産業機械）</td>
+                    <td>TEL:03-5835-3974<br>FAX:03-3865-3086</td>
                   </tr>
                 </tbody>
               </table>
               <table class="c-table sp-block">
                 <tbody>
                   <tr>
-                    <th>北関東営業所・関東営業所</th>
+                    <th>関東</th>
                     <td>〒330-0855 埼玉県さいたま市大宮区上小町1211<a href="https://maps.app.goo.gl/jNSxzvK5o22LayXS9" target="_blank">【マップ】</a></td>
                   </tr>
                   <tr>
                     <th>電話番号</th>
-                    <td>TEL:048-649-7100（産業機械）<br>FAX:048-649-7411（産業機械）<br>TEL:048-644-2147（農業機械）<br>FAX:048-644-9292（農業機械）</td>
+                    <td>TEL:048-649-7100<br>FAX:048-649-7411<br>TEL:048-644-2147<br>FAX:048-644-9292</td>
                   </tr>
                 </tbody>
               </table>
               <table class="c-table sp-block">
                 <tbody>
                   <tr>
-                    <th>長野営業所</th>
-                    <td>〒390-0221 長野県松本市里山辺1659-1<a href="https://maps.app.goo.gl/pf6QbwyoJf6Xm86g7" target="_blank">【マップ】</a></td>
-                  </tr>
-                  <tr>
-                    <th>電話番号</th>
-                    <td>TEL:0263-35-7780（農業機械）<br>FAX:0263-36-5589（農業機械）</td>
-                  </tr>
-                </tbody>
-              </table>
-              <table class="c-table sp-block">
-                <tbody>
-                  <tr>
-                    <th>名古屋営業所</th>
+                    <th>名古屋</th>
                     <td>〒456-0053 愛知県名古屋市熱田区一番3-8-10<a href="https://maps.app.goo.gl/69sdA2mcX4eQPaVq6" target="_blank">【マップ】</a></td>
                   </tr>
                   <tr>
                     <th>電話番号</th>
-                    <td>TEL:052-678-1110（産業機械）<br>TEL:052-678-1120（農業機械）<br>FAX:052-671-0330（共通）</td>
+                    <td>TEL:052-678-1110<br>TEL:052-678-1120<br>FAX:052-671-0330</td>
                   </tr>
                 </tbody>
               </table>
               <table class="c-table sp-block">
                 <tbody>
                   <tr>
-                    <th>静岡営業所</th>
+                    <th>静岡</th>
                     <td>〒424-0205 静岡市清水区興津本町54-1<a href="https://maps.app.goo.gl/jVZ59kUWgy5QWb9k8" target="_blank">【マップ】</a></td>
                   </tr>
                   <tr>
                     <th>電話番号</th>
-                    <td>TEL:054-369-2727（産業機械）<br>TEL:054-369-2742（農業機械）<br>FAX:054-369-2836（共通）</td>
+                    <td>TEL:054-369-2727<br>TEL:054-369-2742<br>FAX:054-369-2836</td>
                   </tr>
                 </tbody>
               </table>
               <table class="c-table sp-block">
                 <tbody>
                   <tr>
-                    <th>広島営業所</th>
+                    <th>広島</th>
                     <td>〒739-1731 広島市安佐北区落合1-19-38<a href="https://maps.app.goo.gl/8eDgTaz82KJkiXQr6" target="_blank">【マップ】</a></td>
                   </tr>
                   <tr>
                     <th>電話番号</th>
-                    <td>TEL:082-843-9015（産業機械）<br>TEL:082-843-9017（農業機械）<br>FAX:082-843-9016（共通）</td>
+                    <td>TEL:082-843-9015<br>TEL:082-843-9017<br>FAX:082-843-9016</td>
                   </tr>
                 </tbody>
               </table>
               <table class="c-table sp-block">
                 <tbody>
                   <tr>
-                    <th>九州営業所</th>
+                    <th>九州</th>
                     <td>〒816-0912 福岡県大野城市御笠川5-9-16<a href="https://maps.app.goo.gl/CwCwR67QoFFrM8Xh8" target="_blank">【マップ】</a></td>
                   </tr>
                   <tr>
                     <th>電話番号</th>
-                    <td>TEL:092-504-7696（産業機械）<br>TEL:092-504-1566（農業機械）<br>FAX:092-504-7424（共通）</td>
+                    <td>TEL:092-504-7696<br>TEL:092-504-1566<br>FAX:092-504-7424</td>
                   </tr>
                 </tbody>
               </table>
@@ -273,7 +261,7 @@
               <table class="c-table sp-block">
                 <tbody>
                   <tr>
-                    <th>大阪サービス（大阪センター）</th>
+                    <th>大阪</th>
                     <td>〒537-0001 大阪市東成区深江北2-3-21<a href="https://maps.app.goo.gl/Yi5SwSUCADRnSzYY7" target="_blank">【マップ】</a></td>
                   </tr>
                   <tr>
@@ -285,7 +273,7 @@
               <table class="c-table sp-block">
                 <tbody>
                   <tr>
-                    <th>関東サービス</th>
+                    <th>関東</th>
                     <td>〒330-0855 埼玉県さいたま市大宮区上小町1211<a href="https://maps.app.goo.gl/M2gmcZ9NSKWyKrj6A" target="_blank">【マップ】</a></td>
                   </tr>
                   <tr>
@@ -299,7 +287,7 @@
               <table class="c-table sp-block">
                 <tbody>
                   <tr>
-                    <th>大阪SCM（大阪センター）</th>
+                    <th>大阪</th>
                     <td>〒537-0001 大阪市東成区深江北2-3-21<a href="https://maps.app.goo.gl/5YvqMg77t2ofeAGBA" target="_blank">【マップ】</a></td>
                   </tr>
                   <tr>
@@ -311,7 +299,7 @@
               <table class="c-table sp-block">
                 <tbody>
                   <tr>
-                    <th>関東SCM</th>
+                    <th>関東</th>
                     <td>〒330-0855 埼玉県さいたま市大宮区上小町1211<a href="https://maps.app.goo.gl/XFb5dkGcdvZFoiEZ8" target="_blank">【マップ】</a></td>
                   </tr>
                   <tr>
@@ -330,9 +318,9 @@
           <div class="gl1 row1">役員会</div>
           <div class="gl1 row2">社長</div>
           <div class="col1">
-            <span class="gl2">経営企画室</span>
             <span class="gl2">商品戦略部</span>
             <span class="gl2">品質保証部</span>
+            <span class="gl2">経営企画室</span>
             <span class="gl2">総務部</span>
             <span class="gl2">人事部</span>
             <span class="gl2">経理部</span>
@@ -342,82 +330,49 @@
             <div class="col">
               <div>
                 <div>
-                  <span class="wh1">営業推進部</span>
+                  <span class="wh1">マーケティング部</span>
                 </div>
                 <div>
-                  <span class="wh1">北海道営業部</span>
+                  <span class="wh1">東日本ブロック</span>
                   <ul>
-                    <li>北海道営業所</li>
+                    <li>北海道グループ</li>
+                    <li>東北グループ</li>
+                    <li>関東第一グループ</li>
+                    <li>関東第二グループ</li>
+                    <li>東京グループ</li>
+                    <li>静岡グループ</li>
                   </ul>
                 </div>
                 <div>
-                  <span class="wh1">産機東部営業部</span>
+                  <span class="wh1">西日本ブロック</span>
                   <ul>
-                    <li>仙台営業所</li>
-                    <li>北関東営業所</li>
-                    <li>東京営業所</li>
-                  </ul>
-                </div>
-                <div>
-                  <span class="wh1">GCH東部営業部</span>
-                  <ul>
-                    <li>東北営業所</li>
-                    <li>関東営業所</li>
-                    <li>長野営業所</li>
-                  </ul>
-                </div>
-                <div>
-                  <span class="wh1">中部営業部</span>
-                  <ul>
-                    <li>静岡営業所</li>
-                    <li>名古屋営業所</li>
+                    <li>名古屋グループ</li>
+                    <li>大阪第一グループ</li>
+                    <li>大阪第二グループ</li>
+                    <li>大阪第三グループ</li>
+                    <li>広島グループ</li>
+                    <li>九州グループ</li>
                   </ul>
                 </div>
               </div>
               <div>
                 <div>
-                  <span class="wh1">産機西部営業部</span>
-                  <ul>
-                    <li>大阪１課</li>
-                    <li>大阪２課</li>
-                    <li>広島営業所</li>
-                  </ul>
-                </div>
-                <div>
-                  <span class="wh1">GCH西部営業部</span>
-                  <ul>
-                    <li>大阪営業所</li>
-                  </ul>
-                </div>
-                <div>
-                  <span class="wh1">九州営業部</span>
-                  <ul>
-                    <li>九州営業所</li>
-                  </ul>
-                </div>
-                <div>
                   <span class="wh1">海外営業部</span>
+                </div>
+                <div>
+                  <span class="wh1">カスタマーサービス部</span>
+                  <ul>
+                    <li>大阪サービスグループ</li>
+                    <li>関東サービスグループ</li>
+                  </ul>
                 </div>
               </div>
             </div>
           </div>
           <div class="col3">
             <span class="gl2">
-              <span>カスタマー<br class="u-show-br-sp">サービス部</span>
-              <ul>
-                <li>大阪サービス<br>　(大阪センター)</li>
-                <li>関東サービス</li>
-              </ul>
-            </span>
-            <span class="gl2">
-              <span>SCM部</span>
-                <ul>
-                  <li>大阪SCM<br>　(大阪センター)</li>
-                  <li>関東SCM</li>
-                </ul>
-            </span>
-            <span class="gl2">
               <span>生産本部</span>
+              <span class="wh1">SCM部</span>
               <span class="wh1">技術開発部</span>
               <span class="wh1">製造部</span>
             </span>
